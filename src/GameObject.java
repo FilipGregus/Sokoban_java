5e59db46526ed672c21ec3ce060541acf182e602
Filip Gregus <filip.gregus2005@gmail.com>
2025-12-06 10:20:31 +0100

    comment game object and player

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,12 +1,26 @@
 import fri.shapesge.Image;
 import fri.shapesge.ImageData;
 
+/**
+ * Trieda reprezentujúca herný objekt v hre
+ * @author Filip Greguš
+ * @version 1.0
+ */
+
 public class GameObject {
     private Position position;
     private ObjectType objectType;
-    private int boxSize;
+    private final int boxSize;
 
     private Image img;
+
+    /**
+     * Konštruktor triedy GameObject inicializuje herný objekt s danou pozíciou, typom a veľkosťou boxu
+     * @param x
+     * @param y
+     * @param objectType
+     * @param boxSize
+     */
 
     public GameObject(int x, int y, ObjectType objectType, int boxSize) {
         this.boxSize = boxSize;
@@ -15,23 +29,52 @@ public class GameObject {
         this.img = null;
     }
 
+    /**
+     * Získanie pozície herného objektu
+     * @return pozícia herného objektu
+     * @author Filip Greguš
+     */
+
     public Position getPosition() {
         return position;
     }
+
+    /**
+     * Nastavenie pozície herného objektu
+     * @param position nová pozícia herného objektu
+     * @author Filip Greguš
+     */
 
     public void setPosition(Position position) {
         this.position = position;
         this.img.changePosition(position.getX() * boxSize, position.getY() * boxSize);
     }
 
+    /**
+     * Získanie typu herného objektu
+     * @return typ herného objektu
+     * @author Filip Greguš
+     */
+
     public ObjectType getObjectType() {
         return objectType;
     }
 
+    /**
+     * Získanie obrázku herného objektu
+     * @return obrázok herného objektu
+     * @author Filip Greguš
+     */
 
     public Image getImg() {
         return img;
     }
+
+    /**
+     * Nastavenie obrázku herného objektu
+     * @param img nový obrázok herného objektu
+     * @author Filip Greguš
+     */
 
     public void setImg(ImageData img) {
         if(this.img == null) {
@@ -41,6 +84,12 @@ public class GameObject {
 
         this.img.changeImage(img);
     }
+
+    /**
+     * Nastavenie typu herného objektu
+     * @param objectType nový typ herného objektu
+     * @author Filip Greguš
+     */
 
     public void setObjectType(ObjectType objectType) {
         this.objectType = objectType;
--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,14 @@
 public class Player {
-    private GameObject playerObject;
-    private GameController gameController;
-    private Movement movement;
+    private final GameObject playerObject;
+    private final GameController gameController;
+    private final Movement movement;
+
+    /**
+     * Konštruktor triedy Player inicializuje hráča s daným herným objektom a herným kontrolérom
+     * @param playerObject herný objekt reprezentujúci hráča
+     * @param gameController herný kontrolér riadiaci logiku hry
+     * @author Filip Greguš
+     */
 
     public Player(GameObject playerObject, GameController gameController) {
         this.playerObject = playerObject;
@@ -11,29 +18,53 @@ public class Player {
         playerObject.getImg().makeVisible();
     }
 
+    /**
+     * Získanie herného objektu reprezentujúceho hráča
+     * @return herný objekt reprezentujúci hráča
+     * @author Filip Greguš
+     */
+
     public GameObject getPlayerObject() {
         return playerObject;
     }
+
+    /**
+     * Metóda na pohyb hráča smerom dole
+     * @author Filip Greguš
+     */
 
     public void moveDown() {
         movement.makeMove(0, 1, this);
         gameController.checkWin();
     }
 
+    /**
+     * Metóda na pohyb hráča smerom hore
+     * @author Filip Greguš
+     */
+
     public void moveUp() {
         movement.makeMove(0, -1, this);
         gameController.checkWin();
     }
+
+    /**
+     * Metóda na pohyb hráča smerom doľava
+     * @author Filip Greguš
+     */
 
     public void moveLeft() {
         movement.makeMove(-1, 0, this);
         gameController.checkWin();
     }
 
+    /**
+     * Metóda na pohyb hráča smerom doprava
+     * @author Filip Greguš
+     */
+
     public void moveRight() {
         movement.makeMove(1, 0, this);
         gameController.checkWin();
     }
-
-
 }
